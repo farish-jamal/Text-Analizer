750d47ab2b743000f8d784d6002e3f2d9362d22c
Farish <developer.websites1@gmail.com>
2021-08-19 10:53:47 +0530

    Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import About from './components/About'
+import React, {useState} from 'react'
+// import About from './components/About'
 import Navbar from './components/Navbar'
-// import Textarea from './components/Textarea'
+import Textarea from './components/Textarea'
 
 function App() {
+    const [mode, setMode] = useState('light')
+
+    const toggleMode = () => {
+      if(mode === 'light'){
+          setMode('dark');
+      }
+      else{
+          setMode('light');
+      }
+    }
     return (
         <>
-            <Navbar title="Text Utils" aboutText="About Us" />
+            <Navbar title="Text Utils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
             <div className="container my-5">
-                {/* <Textarea heading="Enter Text to analize"/> */}
-                <About />
+                <Textarea heading="Enter Text to analize"/>
+                {/* <About /> */}
             </div>
         </>
     )
