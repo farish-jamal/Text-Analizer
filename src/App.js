64aae777a0f71472b0a4020b0b97d767cd8c7309
Farish <developer.websites1@gmail.com>
2021-08-19 10:30:21 +0530

    About Us Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import About from './components/About'
 import Navbar from './components/Navbar'
-import Textarea from './components/Textarea'
+// import Textarea from './components/Textarea'
 
 function App() {
     return (
         <>
             <Navbar title="Text Utils" aboutText="About Us" />
             <div className="container my-5">
-                <Textarea heading="Enter Text to analize"/>
+                {/* <Textarea heading="Enter Text to analize"/> */}
+                <About />
             </div>
         </>
     )
