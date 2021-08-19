f46679911ccb534164546912d916b90b0076a5f7
Farish <developer.websites1@gmail.com>
2021-08-19 12:21:31 +0530

    Dark Mode Enabled

--- src/App.js
+++ src/App.js
@@ -9,16 +9,18 @@ function App() {
     const toggleMode = () => {
       if(mode === 'light'){
           setMode('dark');
+          document.body.style.backgroundColor = '#042743';
       }
       else{
           setMode('light');
+          document.body.style.backgroundColor = 'white';
       }
     }
     return (
         <>
             <Navbar title="Text Utils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
             <div className="container my-5">
-                <Textarea heading="Enter Text to analize"/>
+                <Textarea heading="Enter Text to analize" mode={mode}/>
                 {/* <About /> */}
             </div>
         </>
--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -25,23 +25,27 @@ function Textarea(props) {
     const [text, setText] = useState("Dummy Text Here! Erase it and paste your text here to analize!")
     return (
         <>
-            <div className="container">
-                <h1>{props.heading}</h1>
+            <div className="container text-center" style={{color:props.mode === 'light'? '#042743' : 'white'}}>
+                <h1 className="my-3">{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleChange} rows="8" id="myBox"></textarea>
+                    <textarea className="form-control" value={text} style={{backgroundColor:props.mode === 'light'? 'white' : 'grey', color:props.mode === 'light'? '#042743' : 'white' }} onChange={handleChange} rows="8" id="myBox"></textarea>
                 </div>
-                <div className="btn btn-success my-2" onClick={handleupClick}>To Uppercase</div>
-                <div className="btn btn-success mx-3 my-2" onClick={handledownClick}>To Lowercase</div>
-                <div className="btn btn-success" onClick={handleClearClick}>Text Clear</div>
-                <div className="btn btn-success mx-3" onClick={handleCutClick}>Copy Text</div>
+                <div className="btn btn-primary my-2" onClick={handleupClick}>To Uppercase</div>
+                <div className="btn btn-primary mx-3 my-2" onClick={handledownClick}>To Lowercase</div>
+                <div className="btn btn-primary" onClick={handleClearClick}>Text Clear</div>
+                <div className="btn btn-primary mx-3" onClick={handleCutClick}>Copy Text</div>
             </div>
-            <div className="container my-4 text-center">
+            <div className="container my-4 text-center" style={{color:props.mode === 'light'? '#042743' : 'white'}}>
+                <div className="card my-3" style={{backgroundColor:props.mode === 'light'? 'white' : 'grey'}}>
+                <div className="card-body">
                 <h1>Your Text Summary</h1>
                 <h5>{text.split(" ").length - 1} Words and {text.length} Character</h5>
                 <h5>{0.008 * text.split(" ").length} Minutes Read</h5>
-                <h1>Text Preview</h1>
-                <div className="card">
+                </div>
+                </div>
+                <div className="card" style={{backgroundColor:props.mode === 'light'? 'white' : 'grey'}}>
                     <div className="card-body">
+                <h1>Text Preview</h1>
                         <p className="card-text">{text}</p>
                     </div>
                 </div>
